b1a21bc132a59bfb1d656f502ac8cf2b602665d2
AdeleFabre <adele-fabre@live.fr>
2021-12-14 11:21:05 +0100

    page produit ok

--- front/js/product.js
+++ front/js/product.js
@@ -17,12 +17,15 @@ fetch(`http://localhost:3000/api/products/${productId}`)
         document.querySelector("h1").innerHTML = jsonProductDetail["name"];
         document.querySelector("#price").innerHTML = jsonProductDetail["price"];
         document.querySelector("#description").innerHTML = jsonProductDetail["description"];
-        
+        document.querySelector("div.item__img").innerHTML = `<img src="${jsonProductDetail["imageUrl"]}" alt="${jsonProductDetail["altTxt"]}">`;
+        jsonProductDetail.colors.forEach(element => {
+            let colorOption = document.createElement("option");
+            document.getElementById("colors").appendChild(colorOption);
+            colorOption.innerHTML = `<option value="${element}">${element}</option>`; 
+        });
    
 });
 
-/*fetch(`http://localhost:3000/api/products/${productId}`)
-.then(data => data.json()) 
-.then(jsonProductDetail => {
-    console.log(jsonProductDetail);
-});*/
+
+//jsonProductDetail.colors.forEach(element => {
+  //  document.querySelector("#colors").innerHTML = `<option value="${element}">${element}</option>`;
