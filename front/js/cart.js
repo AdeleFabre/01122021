2e4eb35509be29b19e78ca341030a87f724968ff
AdeleFabre <adele-fabre@live.fr>
2021-12-18 14:08:47 +0100

    Validation inputs formuaire + création objet contact

--- front/js/cart.js
+++ front/js/cart.js
@@ -74,4 +74,109 @@ newQuantityInput.forEach((productsInCart, index) => {
     })
 })
 
+// --------------------- Validation Inputs du formulaire
 
+let formFirstName = document.getElementById("firstName");
+let formLastName = document.getElementById("lastName");
+let formAddress = document.getElementById("address");
+let formCity = document.getElementById("city");
+let formEmail = document.getElementById("email");
+let regExpLetters = /^[a-zA-Zàáâãäæçèéêëìíîïñòóôõöœšùúûü\s\-']*$/;
+let regExpLettersAndNumbers = /^[a-zA-Zàáâãäæçèéêëìíîïñòóôõöœšùúûü0-9\s\-',]*$/;
+let regExpEmail = /^[a-zA-Z0-9.\-_]+[@]{1}[a-zA-Z0-9.\-_]+[.]{1}[a-z]{2,10}$/; 
+
+//Prénom 
+formFirstName.addEventListener('change', function() {
+    let inputToCheck = formFirstName.value;
+    let test = regExpLetters.test(inputToCheck);
+        if(test) {
+            document.getElementById("firstNameErrorMsg").textContent = "";
+        } else {
+            document.getElementById("firstNameErrorMsg").textContent = "Les données renseignées ne sont pas valides";
+        }
+})
+
+//Nom
+formLastName.addEventListener('change', function() {
+    let inputToCheck = formLastName.value;
+    let test = regExpLetters.test(inputToCheck);
+        if(test) {
+            document.getElementById("lastNameErrorMsg").textContent = "";
+        } else {
+            document.getElementById("lastNameErrorMsg").textContent = "Les données renseignées ne sont pas valides";
+        }
+})
+
+//Adresse
+formAddress.addEventListener('change', function() {
+    let inputToCheck = formAddress.value;
+    let test = regExpLettersAndNumbers.test(inputToCheck);
+        if(test) {
+            document.getElementById("addressErrorMsg").textContent = "";
+        } else {
+            document.getElementById("addressErrorMsg").textContent = "Les données renseignées ne sont pas valides";
+        }
+})
+
+//Ville - faut-il permettre un CP ou pas ?
+formCity.addEventListener('change', function() {
+    let inputToCheck = formCity.value;
+    let test = regExpLetters.test(inputToCheck);
+        if(test) {
+            document.getElementById("cityErrorMsg").textContent = "";
+        } else {
+            document.getElementById("cityErrorMsg").textContent = "Les données renseignées ne sont pas valides";
+        }
+})
+
+//Email
+formEmail.addEventListener('change', function() {
+    let inputToCheck = formEmail.value;
+    let test = regExpEmail.test(inputToCheck);
+        if(test) {
+            document.getElementById("emailErrorMsg").textContent = "";
+        } else {
+            document.getElementById("emailErrorMsg").textContent = "L'adresse mail renseignée n'est pas valide";
+        }
+})
+
+
+// -------------------- Validation formulaire
+
+let orderButton = document.getElementById("order");
+
+orderButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    if (regExpLetters.test(formFirstName.value) == true
+    && regExpLetters.test(formLastName.value) == true
+    && regExpLettersAndNumbers.test(formAddress.value) == true
+    && regExpLetters.test(formCity.value) == true
+    && regExpEmail.test(formEmail.value) == true) { 
+        //si tous les champs sont correctement remplis
+        console.log("ok pour création objet");
+        //création tableau de produits
+        let arrayOrder = storage;
+        console.log(arrayOrder);
+        // création objet contact
+        let contact = {
+            Nom: formFirstName.value,
+            Prénom: formLastName.value,
+            Adresse: formAddress.value,
+            Ville: formCity.value,
+            Mail: formEmail.value,
+            Commande: arrayOrder
+        }
+        console.log(contact);
+        //envoyer tout ça sur le local storage ?
+        //redirection vers page confirmation
+        
+
+    } else { //si au moins l'un des champs n'est pas correctement rempli
+        alert("Veuillez intégralement remplir le formulaire avant de soumettre votre commande.");
+    }
+})
+
+
+
+//création objet contact
+//création tableau produit
